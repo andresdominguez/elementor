26984a61ff7498f45cce45593f01d77f430e440b
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-03-30 23:15:31 -0400

    Handle flags and options with minimist

--- bin/elementor.js
+++ bin/elementor.js
@@ -1,5 +1,48 @@
 #!/usr/bin/env node
 
-var elementorServer = require('../lib/server');
+var minimist = require('minimist');
+var path = require('path');
 
-elementorServer.start();
+var args = minimist(process.argv.slice(2), {
+  string: 'chrome',
+  alias: {h: 'help', v: 'version'}
+});
+
+if (args.help) {
+  var cmd = require('path').basename(process.argv[1]);
+  console.log(
+      require('fs')
+          .readFileSync(path.join(__dirname, '../help.txt'), 'utf-8')
+          .replace(/\$0/g, cmd)
+          .trim());
+  process.exit();
+}
+
+if (args.version) {
+  console.log(require('../package.json').version);
+  process.exit();
+}
+
+var getOptions = function() {
+  // Test for flags
+  var options = {};
+
+  // Ignore synchronization?
+  if (args.nonAngular) {
+    console.log('This is not an angular application');
+    options.ignoreSynchronization = true;
+  }
+
+  // Is there a url?
+  if (args._[0]) {
+    options.url = args._[0];
+  }
+
+  return options;
+};
+
+
+
+// Start elementor.
+var options = getOptions();
+require('../lib/server').start(options);
