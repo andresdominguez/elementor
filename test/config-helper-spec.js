a95a41cd8f4b8c6f433dfe6f8f81ed36b87064e8
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-04-02 22:24:58 -0400

    Add test for chrome options

--- test/config-helper-spec.js
+++ test/config-helper-spec.js
@@ -62,7 +62,23 @@ describe('config-helper', function() {
       // Then ensure the extension path was resolved.
       promise.then(function(file) {
         var extensionDir = path.resolve(__dirname, '../extension');
-        ensureChromeOptionsContains(file, "'--load-extension=" + extensionDir + "'");
+        ensureChromeOptionsContains(file,
+            "'--load-extension=" + extensionDir + "'");
+        done();
+      });
+    });
+
+    it('should add chrome options', function(done) {
+      // When you create a config file with chrome options.
+      var promise = configHelper.createProtractorConfig({
+        chromeOptions: '--remote-debugging-port=9222 --disable-web-security'
+      });
+
+      // Then ensure the options are passed to the output file.
+      promise.then(function(file) {
+        ensureChromeOptionsContains(file,
+            "'--remote-debugging-port=9222', '--disable-web-security"
+        );
         done();
       });
     });
